80cb8ae8fc9d77172b3d5a4dbf6f09d3381cd63c
kbg <khddhk123@naver.com>
2025-05-28 01:04:54 +0900

    list btn태그 div로 변경 이에따른css수정

--- hidden_door-react/src/components/cs/faq/FaqItem.jsx
+++ hidden_door-react/src/components/cs/faq/FaqItem.jsx
@@ -11,9 +11,17 @@ const FaqItem = ({ faq, page, search }) => {
 
   return (
     <li className="faq-tr-content">
-      <button onClick={handleDetail} className="title-div">
+      <div
+        onClick={handleDetail}
+        className="title-div"
+        role="button"
+        tabIndex={0}
+        onKeyDown={(e) => {
+          if (e.key === "Enter" || e.key === " ") handleDetail();
+        }}
+      >
         {faq.title}
-      </button>
+      </div>
       <div className="cs-date">{faq.kstCreDate}</div>
     </li>
   );
